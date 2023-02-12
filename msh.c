fd85c67266bb0445e9b774861845a09d23f96c52
Ikechukwu Ofili <62239576+ikeofilic1@users.noreply.github.com>
2023-02-12 05:59:37 +0000

    Ignores empty lines and parse in function

--- msh.c
+++ msh.c
@@ -1,18 +1,18 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2016 Trevor Bakker 
-// 
+//
+// Copyright (c) 2016 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// 
+//
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,90 +31,89 @@
 #include <string.h>
 #include <signal.h>
 
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
-                                // so we need to define what delimits our tokens.
-                                // In this case  white space
-                                // will separate the tokens on our command line
+#define WHITESPACE " \t\n" // We want to split our command line up into tokens
+                           // so we need to define what delimits our tokens.
+                           // In this case  white space
+                           // will separate the tokens on our command line
 
-#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+#define MAX_COMMAND_SIZE 255 // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports four arguments
+#define MAX_NUM_ARGUMENTS 5 // Mav shell only supports four arguments
+
+char *token[MAX_NUM_ARGUMENTS + 1];
+
+/* Parse input*/
+void parse_tokens(const char *command_string) {
+  int token_count = 0;
+
+  // Pointer to point to the token parsed by strsep
+  char *argument_ptr = NULL;
+
+  char *working_string = strdup(command_string);
+
+  // we are going to move the working_string pointer so
+  // keep track of its original value so we can deallocate
+  // the correct amount at the end
+  char *head_ptr = working_string;
+
+  // Tokenize the input strings with whitespace used as the delimiter
+  while (((argument_ptr = strsep(&working_string, WHITESPACE)) != NULL) &&
+          (token_count < MAX_NUM_ARGUMENTS))
+  {
+    token[token_count] = strndup(argument_ptr, MAX_COMMAND_SIZE);
+    if (strlen(token[token_count]) == 0)
+    {
+      token[token_count] = NULL;
+    }
+    token_count++;
+  }
+
+  while (token_count <= MAX_NUM_ARGUMENTS)
+  {
+    token[token_count++] = NULL;
+  }
+
+  free(head_ptr);
+}
 
 int main()
 {
+  int c;
+  char *command_string = (char *)malloc(MAX_COMMAND_SIZE);
 
-  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+  size_t hist_ptr = 0;
 
-  while( 1 )
+  while (1)
   {
     // Print out the msh prompt
-    printf ("msh> ");
+    printf("msh> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
     // This while command will wait here until the user
     // inputs something since fgets returns NULL when there
     // is no input
-    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
+    while (!fgets(command_string, MAX_COMMAND_SIZE, stdin));
+
+    if (*command_string == '\n')
+    {
+      continue;
+    }
 
     /* Parse input */
-    char *token[MAX_NUM_ARGUMENTS];
-
-    for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
-    {
-      token[i] = NULL;
-    }
-
-    int   token_count = 0;                                 
-                                                           
-    // Pointer to point to the token
-    // parsed by strsep
-    char *argument_ptr = NULL;                                         
-                                                           
-    char *working_string  = strdup( command_string );                
-
-    // we are going to move the working_string pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
-    char *head_ptr = working_string;
-
-    // Tokenize the input strings with whitespace used as the delimiter
-    while ( ( (argument_ptr = strsep(&working_string, WHITESPACE ) ) != NULL) && 
-              (token_count<MAX_NUM_ARGUMENTS))
-    {
-      token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
-      {
-        token[token_count] = NULL;
-      }
-        token_count++;
-    }
-
-    // Now print the tokenized input as a debug check
-    // \TODO Remove this for loop and replace with your shell functionality
-
-    int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
-    {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
-    }
-
-
+    parse_tokens(command_string);
+    
     // Cleanup allocated memory
-    for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
+    for (int i = 0; i < MAX_NUM_ARGUMENTS; i++)
     {
-      if( token[i] != NULL )
+      if (token[i] != NULL)
       {
-        free( token[i] );
+        free(token[i]);
       }
     }
-
-    free( head_ptr );
-
   }
 
-  free( command_string );
+  free(command_string);
 
   return 0;
-  // e2520ca2-76f3-90d6-0242ac120003
 }
